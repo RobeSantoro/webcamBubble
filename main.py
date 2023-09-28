e4c58d101b04f57e769e44441f0ce316bc7845bb
RobeSantoro <santoro.robe@gmail.com>
2023-09-28 22:50:12 +0200

    Fix white jagged border

--- main.py
+++ main.py
@@ -7,36 +7,43 @@ import numpy as np
 class WebcamBubbleApp:
     def __init__(self):
 
+        # Create the root window
         self.root = Tk()
         self.root.overrideredirect(1)
-        self.root.configure(bg="white")
+        self.root.configure(bg="black")
         self.root.attributes("-topmost", 1)
-        self.root.attributes("-transparentcolor", "white")
-        self.root.geometry("500x500")
-
-        self.label = Label(self.root, height=500,
-                           width=500, bg="white", border=0)
-        self.label.pack()
-
-        self.capture = cv2.VideoCapture(0)
-
-        img = Image.open("bg.png")
-        img = img.resize((300, 300), Image.ADAPTIVE)
-        self.image = ImageTk.PhotoImage(img)
-        self.label.configure(image=self.image)
+        self.root.attributes("-transparentcolor", "black")
+        self.root.geometry("350x350")
 
         # Initialize variables for dragging
         self.drag_data = {"x": 0, "y": 0, "clicked": False}
 
+        # Load the image
+        img = Image.open("bg.png")
+        img = img.resize((300, 300), Image.ADAPTIVE)
+        self.image = ImageTk.PhotoImage(img)
+
         # Bind mouse events for window dragging
+        self.label = Label(self.root,
+                           height=350,
+                           width=350,
+                           bg="black",
+                           border=0
+                           )
+        self.label.configure(image=self.image)
         self.label.bind("<ButtonPress-1>", self.start_drag)
         self.label.bind("<ButtonRelease-1>", self.stop_drag)
         self.label.bind("<B1-Motion>", self.on_drag)
+        
+        self.label.pack()
 
+        # Create a capture object
+        self.capture = cv2.VideoCapture(0)
         self.update()
 
     def update(self):
         ret, frame = self.capture.read()
+
         if ret:
             # Flip the frame horizontally
             frame = cv2.flip(frame, 1)
@@ -65,10 +72,10 @@ class WebcamBubbleApp:
             frame_image = Image.fromarray(frame)
 
             # Resize the frame_image to fit the application window
-            frame_image = frame_image.resize((500, 500))
+            frame_image = frame_image.resize((300, 300))
 
             # Create an alpha mask from the numpy mask array
-            alpha_mask = Image.fromarray(mask).resize((500, 500))
+            alpha_mask = Image.fromarray(mask).resize((300, 300))
 
             # Convert the frame_image into "RGBA" mode and add the alpha_mask as the alpha channel
             frame_image = frame_image.convert("RGBA")
@@ -99,7 +106,7 @@ class WebcamBubbleApp:
             # Move the window to the new position
             x_pos = self.root.winfo_x() + deltax
             y_pos = self.root.winfo_y() + deltay
-            self.root.wm_geometry(f"500x500+{x_pos}+{y_pos}")
+            self.root.wm_geometry(f"350x350+{x_pos}+{y_pos}")
 
     def run(self):
         self.root.mainloop()
