7c34581258326a702b59da73666067bcfd324513
RobeSantoro <santoro.robe@gmail.com>
2023-09-29 13:00:45 +0200

    Add Button to open settings window

--- main.py
+++ main.py
@@ -1,41 +1,65 @@
-from tkinter import *
-from PIL import ImageTk, Image
 import cv2
 import numpy as np
+import tkinter as tk
+import customtkinter as ctk
+
+from PIL import Image, ImageTk
 
 
 class WebcamBubbleApp:
     def __init__(self):
 
+        self.size = 300
+        self.margin = 50
+
         # Create the root window
-        self.root = Tk()
+        self.root = tk.Tk()
         self.root.overrideredirect(1)
         self.root.configure(bg="black")
         self.root.attributes("-topmost", 1)
         self.root.attributes("-transparentcolor", "black")
-        self.root.geometry("350x350")
+        self.root.geometry(
+            f"{self.size}x{self.size}+{self.margin}+{self.margin}")
+
+        # Get the screen size
+        self.screen_width = self.root.winfo_screenwidth()
+        self.screen_height = self.root.winfo_screenheight()
+        print(self.screen_width, self.screen_height)
 
         # Initialize variables for dragging
         self.drag_data = {"x": 0, "y": 0, "clicked": False}
 
         # Load the image
         img = Image.open("bg.png")
-        img = img.resize((300, 300), Image.ADAPTIVE)
+        img = img.resize((self.size, self.size), Image.ADAPTIVE)
         self.image = ImageTk.PhotoImage(img)
 
         # Bind mouse events for window dragging
-        self.label = Label(self.root,
-                           height=350,
-                           width=350,
+        self.label = tk.Label(self.root,
+                           height=self.size+self.margin,
+                           width=self.size+self.margin,
                            bg="black",
                            border=0
                            )
         self.label.configure(image=self.image)
+        self.label.pack()
+
+        # Add a button that opens a new window
+        self.open_button = tk.Button(
+            self.root, text="Open", command=self.open_settings_window)
+
+        # Place the button along the circumference of the circle
+        self.open_button.place(
+            relx=0.85,
+            rely=0.85,
+            anchor="center",
+            width=50,
+            height=50)
+
+        # Bind mouse events for window dragging
         self.label.bind("<ButtonPress-1>", self.start_drag)
         self.label.bind("<ButtonRelease-1>", self.stop_drag)
         self.label.bind("<B1-Motion>", self.on_drag)
-        
-        self.label.pack()
 
         # Create a capture object
         self.capture = cv2.VideoCapture(0)
@@ -72,10 +96,10 @@ class WebcamBubbleApp:
             frame_image = Image.fromarray(frame)
 
             # Resize the frame_image to fit the application window
-            frame_image = frame_image.resize((300, 300))
+            frame_image = frame_image.resize((self.size, self.size))
 
             # Create an alpha mask from the numpy mask array
-            alpha_mask = Image.fromarray(mask).resize((300, 300))
+            alpha_mask = Image.fromarray(mask).resize((self.size, self.size))
 
             # Convert the frame_image into "RGBA" mode and add the alpha_mask as the alpha channel
             frame_image = frame_image.convert("RGBA")
@@ -87,7 +111,20 @@ class WebcamBubbleApp:
             self.label.configure(image=frame_image)
             self.label.image = frame_image
 
-        self.root.after(10, self.update)
+        self.root.after(30, self.update)
+
+    def open_settings_window(self):
+
+        size = 300
+
+        # Create the settings window
+        settings_window = tk.Toplevel(self.root)
+        settings_window.attributes("-topmost", 1)
+        settings_window.title("Settings")
+        settings_window.geometry(f"{size}x{size}")
+
+        # Center the window on the screen 
+        settings_window.wm_geometry(f"+{self.screen_width//2-size//2}+{self.screen_height//2-size//2}")
 
     def start_drag(self, event):
         self.drag_data["x"] = event.x
